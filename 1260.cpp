80a26ea3008dff68a2a4e607e77dd8fac14fc392
younghun77 <167798148+younghun77@users.noreply.github.com>
2024-05-12 05:05:08 +0900

    Update 1260.cpp

--- 1260.cpp
+++ 1260.cpp
@@ -5,9 +5,58 @@
 #include <vector>
 
 using namespace std;
+vector<int> graph[1001];
+bool visited[1001];
 
-int main()
+void dfs(int start)
 {
+    visited[start] = true;
+    for (int i=0; i<graph[start].size(); i++)
+    {
+      int y = graph[start][i];
+      if(!visited[y]) dfs(y);
+    }
+}
+void bfs(int start)
+{
+    queue<int> que;
+    que.push(start);
+    visited[start] = true;
+    while(!que.empty())
+    {
+      que.pop();
+      int x = que.front();
+      for (int i=0; i<graph[x].size() < i++)
+      {
+          int y = graph[x][i];
+          if (visited[y]) continue;
+          visited[y] = true;
+          que.push(y);
+      }
+    }
+}
 
-  
+
+int main() {
+	cin.tie(NULL);
+	ios_base::sync_with_stdio(false);
+	int v;
+	cin >> n >> m >> v;
+
+	int numFrom, numTo;
+	for (int i = 0; i < m; i++) {
+		cin >> numFrom >> numTo;
+    //정점<->간선
+		graph[numFrom].push_back(numTo);
+		graph[numTo].push_back(numFrom);
+	}
+
+  //각 정점이 가지고 있는 간선에 대해서 sort
+	for (int i = 0; i < n; i++){
+		sort(graph[i].begin(), graph[i].end());
+	}
+	dfs(v);
+	cout << endl;
+	bfs(v);
+	cout << endl;
 }
