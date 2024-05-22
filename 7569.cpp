fa49358a1b24aee557217d9df890a5f6264e08ec
younghun77 <167798148+younghun77@users.noreply.github.com>
2024-05-23 02:01:43 +0900

    Update 7569.cpp

--- 7569.cpp
+++ 7569.cpp
@@ -6,38 +6,38 @@
 #include <vector>
 #include <tuple>
 
+using namespace std;
+int M, N, H;
 bool visited[101][101][101];
+int arr[101][101][101];
 int dx[] = {-1, 1, 0, 0, 0, 0};
 int dy[] = {0, 0, -1, 1, 0, 0};
 int dz[] = {0, 0, 0, 0, -1, 1};
 queue<tuple<int, int, int>> que;
 vector<int> vec;
-//1.익은 복숭아는 여러곳에 위치함
-//2.익은 복숭아를 기준으로 상하좌우 이동하면서 visited 인지 확인 후 아니면 증가
-//3.start 지점부터 가장 많은 count가 답이 됨
 
 int bfs(int x, int y, int z)
 {
-    int curx, cury, curz;
-    int count;
-    que.push({x, y, z})
+    int count = 0;
+    que.push({x, y, z});
     visited[x][y][z] = true;
 
-    while(!que.empty())
+    while (!que.empty())
     {
-        std::tie(curx, cury, curz) = que.front();
+        int curx, cury, curz;
+        tie(curx, cury, curz) = que.front();
         que.pop();
-        visited[curx][cury][curz] = true;
 
-        for (int i=0; i<6; i++)
+        for (int i = 0; i < 6; i++)
         {
-            int dx = curx+dx[i];
-            int dy = cury+dy[i];
-            int dz = curz+dz[i]
-            if (dx < 2 || dy < 2 || dz < 2 || dx > 100 || dy > 100 || dz > 100) continue;
-            if (arr[dx][dy][dz] == 0 && !visited[dx][dy][dz])
+            int nx = curx + dx[i];
+            int ny = cury + dy[i];
+            int nz = curz + dz[i];
+            if (nx < 0 || ny < 0 || nz < 0 || nx >= M || ny >= N || nz >= H) continue;
+            if (arr[nx][ny][nz] == 0 && !visited[nx][ny][nz])
             {
-                que.push({dx,dy,dz});
+                que.push({nx, ny, nz});
+                visited[nx][ny][nz] = true;
                 count++;
             }
         }
@@ -47,13 +47,25 @@ int bfs(int x, int y, int z)
 
 int main()
 {
-    for(int i=0; i<N; i++) 
-    for (int j=0; j<M; j++)
-    for (int k=0; k<H; k++)
-    if (!visited(arr[i][j][k]) && arr[i][j][k] == 1) 
+    cin >> M >> N >> H;
+
+    for (int i = 0; i < H; i++) 
     {
-        vec.push_back(bfs(i, j, k));
+        for (int j = 0; j < N; j++)
+        {
+            for (int k = 0; k < M; k++)
+            {
+                int temp;
+                cin >> temp;
+                arr[k][j][i] = temp;
+                visited[k][j][i] = false;
+                if (!visited[k][j][i] && arr[k][j][i] == 1) 
+                {
+                    vec.push_back(bfs(k, j, i));
+                }
+            }
+        }
     }
     sort(vec.begin(), vec.end());
-    cout << vec[vec.size()];
+    cout << vec.back();
 }
